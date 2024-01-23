b57abf50e58483def41789e1bd0e1383950e57cb
Administrator <1152449961@qq.com>
2024-01-23 23:54:09 +0800

    初始化

---
+++ src/main/java/com/lang/common_server/CommonServerApplication.java
@@ -0,0 +1,15 @@
+package com.lang.common_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonServerApplication {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lang/common_server/CommonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lang.common_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
